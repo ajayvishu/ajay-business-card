bb786687f1089f94db8bf8be14fcf1ef5a7943f0
ajay vishwakarma <40162397+ajayvishu@users.noreply.github.com>
2023-12-26 16:20:52 +0530

    Update business-card.js
    code optimized

--- bin/business-card.js
+++ bin/business-card.js
@@ -1,27 +1,59 @@
 #!/usr/bin/env node
 
+// Import necessary modules
 import boxen from "boxen";
 import chalk from "chalk";
 
+// Function to calculate years of experience
+const calculateExperience = (startYear) => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - startYear;
+};
+
+// Set the start year of your experience
+const startYearOfExperience = 2019; // Replace with your actual start year
+
+// Calculate years of experience
+const yearsOfExperience = calculateExperience(startYearOfExperience);
+
+// Define boxenOptions for the business card
 const boxenOptions = {
   padding: 1,
   width: 54,
-  title: "software engineer",
+  title: "Software Engineer",
   titleAlignment: "center",
   borderStyle: "double",
   borderColor: "#ffb626",
 };
 
+// Create a bold introduction text
 const intro = chalk.bold(
-  "Hello! I am ajay vishwakarma, an experienced Web Application Developer with a passion for crafting high-performance and scalable web applications. With a dedicated focus spanning over 4 years, I have honed my expertise in various technologies, ensuring robust solutions that stand the test of both functionality and efficiency.\n\n",
+  `Hello, I'm Ajay Vishwakarma, a seasoned Software Engineer with ${yearsOfExperience} years of expertise in crafting high-performance web applications. Proficient in a diverse range of technologies, I excel in delivering scalable solutions that align with business goals. Committed to continuous learning, I stay ahead of industry trends, ensuring innovative and efficient solutions. My collaborative approach and analytical mindset contribute to successful project outcomes. Explore more about my work on my...` +
+  "\n\n"
 );
 
+// Define the links object
 const links = [
-  { name: chalk.hex("#fff").bgHex("#582be8")("Website"), url: "   https://www.ajayvishu.in/" },
-  { name: chalk.hex("#fff").bgHex("#000")("DEV.to"), url: "    https://dev.to/ajayvishwakarma" },
-  { name: chalk.hex("#fff").bgHex("#6e5494")("GitHub"), url: "    https://github.com/ajayvishu" },
+  {
+    name: chalk.hex("#fff").bgHex("#582be8").bold("Website"),
+    url: "https://www.ajayvishu.in/",
+  },
+  {
+    name: chalk.hex("#fff").bgHex("#000").bold("DEV.to"),
+    url: "https://dev.to/ajayvishwakarma",
+  },
+  {
+    name: chalk.hex("#fff").bgHex("#6e5494").bold("GitHub"),
+    url: "https://github.com/ajayvishu",
+  },
 ];
 
+// Create a string of link names and URLs
 const linkList = links.map((link) => `${link.name} ${link.url}`).join("\n");
 
-console.log(boxen(intro + linkList, boxenOptions));
+try {
+  // Print the business card using boxen
+  console.log(boxen(intro + linkList, boxenOptions));
+} catch (error) {
+  console.error("An error occurred: ", error.message);
+}
